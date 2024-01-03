f13cb3b83cf23fe0e5aa4eadf26020d94f5e40d6
Clover <ms.greenclover13@gmail.com>
2024-01-03 16:45:35 +0630

    add linked list

--- data-structure/linked-list/code.js
+++ data-structure/linked-list/code.js
@@ -1,38 +1,101 @@
-class Node{
-    constructor(value){
+class Node {
+    constructor(value) {
         this.value = value
         this.next = null
     }
 }
 
-class LinkedList{
-    constructor(){
+class LinkedList {
+    constructor() {
         this.head = null
         this.size = 0
     }
 
-    is_empty(){
+    is_empty() {
         return this.size === 0
     }
 
-    prepend(value){
-        const node = new Node(value)
-        if(this.is_empty()){
+    prepend(value) {
+        let node = new Node(value)
+        if (this.is_empty()) {
             this.head = node
-        }else{
+        } else {
             node.next = this.head
             this.head = node
         }
         this.size++
     }
+
+    append(value) {
+        let node = new Node(value)
+        if (this.is_empty()) {
+            this.head = node
+        } else {
+            let current = this.head
+            while (current.next) {
+                current = current.next
+            }
+            current.next = node
+        }
+        this.size++
+    }
+
+    insert(position, value) {
+        if (position < 0 || position > this.size) {
+            return false
+        }
+
+        if (position === 0) {
+            return this.prepend(value)
+        } else {
+            let node = new Node(value)
+            let current = this.head;
+            let prev = null
+            let i = 0;
+            while (i < position) {
+                prev = current
+                current = current.next
+                i++
+            }
+            node.next = current
+            prev.next = node
+            this.size++
+        }
+    }
+
+    index_of(element){
+        let current = this.head
+        let i = 0
+        while(current){
+            if(current.value === element){
+                return i
+            }
+            i++
+            current = current.next
+        }
+        return -1
+    }
+
+    print(){
+        let current = this.head
+        let str = ''
+        while(current){
+            str += current.value + ' ' 
+            current = current.next
+        }
+        return str
+    }
 }
 
 let list = new LinkedList()
-list.prepend("D")
-list.prepend("C")
-list.prepend("B")
-list.prepend("A")
-
+// list.prepend("D")
+// list.prepend("C")
+// list.prepend("B")
+// list.prepend("A")
+list.append("A")
+list.append("B")
+list.append("E")
+console.log(list.insert(2, "C"))
 console.log(list.head)
-console.log(list.size)
-
+console.log(list.index_of('D'))
+console.log(list.print())
