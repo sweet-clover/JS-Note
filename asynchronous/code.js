2552f1e672b373abb0ad821a0fe725243ab2e0fb
Clover <ms.greenclover13@gmail.com>
2024-01-20 14:21:34 +0630

    add synchronous

---
+++ asynchronous/code.js
@@ -0,0 +1,20 @@
+//synchronous javascript
+
+console.log("Role 1")
+console.log("Role 2")
+console.log("Role 3")
+
+//asynchronous javascript
+
+function takeExam(role) {
+    let time = Math.floor(Math.random() * 1000);
+    console.log("time", time)
+    setTimeout(function() {
+        console.log(role + ' done!');
+    }, time);
+}
+
+Math.random()
+takeExam("Role 1")
+takeExam("Role 2")
+takeExam("Role 3")
